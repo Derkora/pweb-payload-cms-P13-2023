cb0f5c601aa8e40e7b1c16afd5806b5f6d7299c4
HafizSantosa <myhafizas@gmail.com>
2023-10-23 18:04:57 +0700

    pus

--- payload-cms/src/collections/Category.js
+++ payload-cms/src/collections/Category.js
@@ -1,5 +1,12 @@
 /** @type {import('payload/types').CollectionConfig} */
 const Category = {
     slug : "Category",
+    fields : [
+        {
+          name: "Kategori",
+          type: "text",
+          required: true,
+        },
+      ]
 }
 export default Category
--- payload-cms/src/collections/Todo.js
+++ payload-cms/src/collections/Todo.js
@@ -1,6 +1,41 @@
 /** @type {import('payload/types').CollectionConfig} */
 const Todo = {
-    slug : "Todo",
-}
-    export default Todo
-
+    slug: "Todo",
+    access: {
+      read: () => true,
+      update: () => true,
+      delete: () => true,
+      create: () => true,
+    },
+    fields: [
+      {
+        name: "Nama Tugas",
+        type: "text",
+        required: true,
+      },
+      {
+        name: 'Tanggal',
+        type: 'date',
+        admin: {
+          date: {
+              pickerAppearance: 'dayOnly',
+              displayFormat: 'd MMM yyy',
+          },
+        },
+      },
+      {
+        name: 'Kategori',
+        type: 'relationship',
+        required: true,
+        relationTo: 'Category',
+        filterOptions: ({ relationTo, siblingData }) => {
+          return {
+            division: { equals: 'Kuliah' }
+          };
+        },
+      },
+    ],
+  };
+  
+  export default Todo;
+  
